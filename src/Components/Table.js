bd26d4846021e2b93e4e75d513a2b25c42a4e1f4
Lucky Coder <muxsin.shokirov@gmail.com>
2021-06-04 05:39:30 +0700

    Props

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
 
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = (props) => {
+  let rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button>Remove</button>
+        </td>
+      </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>;
+};
+
 class Table extends Component {
   render() {
+    const { characterData } = this.props;
+
     return (
       <table>
-        <thead>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Remove</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Muhsin</td>
-            <td>Programmer</td>
-            <td>
-              <button>Remove</button>
-            </td>
-          </tr>
-        </tbody>
+        <TableHeader />
+        <TableBody characterData={characterData} />
       </table>
     );
   }
